597b85d51c98f1c28c4df5e3d4c75d1d71c3d581
Jaya <jaya.mali@sparkegnite.com>
2026-06-23 19:18:50 +0530

    Update APex Classes as per code REview

--- force-app/main/default/lwc/showduplicateobjectrecords/showduplicateobjectrecords.js
+++ force-app/main/default/lwc/showduplicateobjectrecords/showduplicateobjectrecords.js
@@ -4,50 +4,22 @@ import { NavigationMixin } from 'lightning/navigation';
 export default class Showduplicateobjectrecords extends NavigationMixin(LightningElement) {
 
     duplicateRecords = [];
-    
-        @api
-        get value() {
-              console.log('Inside get value');
-            return this._value;
-        }
-    
-        set value(value) {
-            console.log('Inside set value');
-            this._value = value;
-    
-            console.log(
-                'Agent Data:',
-                JSON.stringify(value)
-            );
-    
-            
-        }
-    
-         connectedCallback() {
-              console.log('Before this.value :: ' , JSON.stringify( this.value));
-             console.log('Before lwc called :: ' , JSON.stringify( this.duplicateRecords));
-            this.duplicateRecords = this.value.lstDuplicateRecordInfo;
-               console.log('After lwc called :: ' , JSON.stringify( this.duplicateRecords));
-         }
 
-        /*handleViewRecord(event) {
-            console.log('handleViewRecord:: ');
-            const recordId = event.target.dataset.id;
-               console.log('handleViewRecord:: ' + recordId);
-            this[NavigationMixin.Navigate]({
-                type: 'standard__recordPage',
-                attributes: {
-                    recordId: recordId,
-                    objectApiName: 'Account',
-                    actionName: 'view'
-                }
-            });
-        }*/
+    @api
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        this._value = value;
+    }
+
+    connectedCallback() {
+        this.duplicateRecords = this.value.lstDuplicateRecordInfo;
+    }
 
     handleViewRecord(event) {
-         console.log('handleViewRecord:: ');
-            const recordId = event.target.dataset.id;
-               console.log('handleViewRecord:: ' + recordId);
+        const recordId = event.target.dataset.id;
         window.open('/' + recordId, '_blank');
     }
 
